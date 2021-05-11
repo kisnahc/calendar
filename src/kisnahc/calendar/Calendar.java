b3a8a0905f776b0572ae02df3e409252e6ebc9ba
kisnahc <kisnahc@naver.com>
2021-05-11 16:43:13 +0900

    refactoring Calendar class.

--- src/kisnahc/calendar/Calendar.java
+++ src/kisnahc/calendar/Calendar.java
@@ -23,33 +23,54 @@ public class Calendar {
 
     }
 
-    public void printCalendar(int inputYear, int inputMonth, Days days) {
+    public void printCalendar(int inputYear, int inputMonth) {
+        /**
+         * 기준일 - 1583년(그레고리력이 시행된 년도) 1월 1일 토요일
+         */
+        int sum = 0;
+        for (int i = 1583; i < inputYear; i++) {
+            if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) {
+                sum += 2;
+            } else {
+                sum += 1;
+            }
+        }
+        int firstDay = (sum + 6) % 7; // 1월 1일 토요일.
+
+        for (int j = 1; j < inputMonth; j++) {
+            firstDay += getMonthDays(inputYear, j) % 7;
+        }
+
+        int day = firstDay % 7;
+        int num = 1;
+        int max = getMonthDays(inputYear, inputMonth);
+        boolean start = false;
+
         System.out.println("   <<" + inputYear + "년" + "  " + inputMonth + "월>>" + "\n" +
                 " SU MO TU WE TH FR SA" + "\n" +
                 " --------------------");
 
-        int monthDays = getMonthDays(inputYear, inputMonth);
+        loop:
+        for (int row = 0; row <= 5; row++) {
+            for (int column = 0; column <= 6; column++) {
+                if (row == 0 && !start) {
+                    if (column == day) {
+                        start = true;
+                    } else {
+                        System.out.print("   ");
+                        continue;
+                    }
+                }
 
-        for (int i = 0; i < days.getSpaceCount(); i++) {
-            System.out.print(" ");
-        }
+                System.out.printf("%3d", num);
+                num++;
 
-        int start = 1;
-        for (int i = 0; i < 7 - days.getSpaceCount() / 3; i++) {
-            System.out.printf("%3d", start);
-            start++;
-        }
-        System.out.println();
-
-        int j = 0;
-        for (int i = start; i <= monthDays; i++) {
-            System.out.printf("%3d", i);
-            j++;
-            if (j % 7 == 0) {
-                System.out.println();
+                if (num > max) {
+                    break loop;
+                }
             }
+            System.out.println("");
         }
-        System.out.println();
+
     }
-    //기능수행 목적.
 }
--- src/kisnahc/calendar/Prompt.java
+++ src/kisnahc/calendar/Prompt.java
@@ -6,16 +6,13 @@ public class Prompt {
 
     private static final String YEAR_PROMPT = "year> ";
     private static final String PROMPT = "cal> ";
-    private static final String DAY = "day> ";
 
     public void runPrompt() {
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
 
-
         int inputMonth;
         int inputYear;
-        String inputDay;
 
         while (true) {
             System.out.println("년도를 입력하세요.");
@@ -26,23 +23,17 @@ public class Prompt {
             System.out.print(PROMPT);
             inputMonth = Integer.parseInt(scanner.nextLine());
 
-            System.out.println("첫번째 요일을 입력하세요. ex) SU, MO, TU, WE, TH, FR, SA");
-            System.out.print(DAY);
-            inputDay = scanner.nextLine();
-
-            //inputDay.trim();
-            Days days = Days.valueOf(inputDay);
-            //Days days = Days.MO;
-
-
             if (inputMonth == -1) {
+                System.out.println("Bye ~ :)");
                 break;
             } else if (inputMonth > 12) {
+                System.out.println("cal은 1 ~ 12월 사이를 입력하세요." + "\n");
                 continue;
             }
-            cal.printCalendar(inputYear, inputMonth, days);
+            cal.printCalendar(inputYear, inputMonth);
+            System.out.println();
+
         }
-        System.out.println("Have a nice day~:)");
         scanner.close();
     }
 
